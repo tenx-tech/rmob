80c4d1c0bb11053dd2cbec199c5c48db4e3c815c
Florian Peter <florian.peter@gmx.at>
2019-03-13 22:05:12 +0800

    Factor out some stuff. #master

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ extern crate clap;
 
 use clap::{App, Arg, SubCommand};
 use std::fs;
+use std::os::unix::fs::OpenOptionsExt;
 use std::path::Path;
 use std::process::Command;
-use std::os::unix::fs::OpenOptionsExt;
 
 fn main() {
     let matches = App::new("Rmob")
@@ -30,22 +30,7 @@ fn main() {
 
     if let Some(matches) = matches.subcommand_matches("prepare-commit-msg") {
         let commit_message_file = matches.value_of("COMMIT_MESSAGE_FILE").unwrap();
-        let commit_message =
-            fs::read_to_string(commit_message_file).expect("Couldn't read git's COMMIT_EDITMSG");
-
-        let mob_cmd = Command::new("git")
-            .arg("mob-print")
-            .output()
-            .expect("for now we still depend on git mob-print")
-            .clone()
-            .stdout;
-        let mob = String::from_utf8_lossy(&mob_cmd);
-
-        let comment_pos = commit_message.find("# ").unwrap();
-        let (git_message, git_comments) = commit_message.split_at(comment_pos);
-        let updated_message = format!("{}{}{}", git_message, mob, git_comments);
-
-        fs::write(commit_message_file, updated_message).expect("Error writing commit message file");
+        inject_into_commit_message_file(commit_message_file);
     }
 
     if let Some(matches) = matches.subcommand_matches("init") {
@@ -55,23 +40,44 @@ fn main() {
         if Path::new(hook_file).exists() {
             println!("You have an existing prepare-commit-msg hook, which we need to overwrite. Please back it up and remove it!");
         } else {
-            let hook_code = "#!/bin/bash
+            create_hook(hook_file);
+        }
+    }
+}
+
+fn create_hook(hook_file: &str) {
+    let hook_code = "#!/bin/bash
 
 rmob prepare-commit-msg \"$1\"";
 
-            // TODO: Make OS-agnostic
-            fs::OpenOptions::new()
-                .create(true)
-                .write(true)
-                .mode(0o775)
-                .open(hook_file)
-                .unwrap();
+    write_executable(hook_file, hook_code);
 
-            fs::write(hook_file, hook_code)
-                .expect("Failed to write git hook");
+    println!("Success!");
+}
 
+// TODO: Make OS-agnostic
+fn write_executable(file: &str, contents: &str) {
+    fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .mode(0o775)
+        .open(file)
+        .unwrap();
+    fs::write(file, contents).expect("Failed to write git hook");
+}
 
-            println!("Success!");
-        }
-    }
+fn inject_into_commit_message_file(commit_message_file: &str) {
+    let commit_message =
+        fs::read_to_string(commit_message_file).expect("Couldn't read git's COMMIT_EDITMSG");
+    let mob_cmd = Command::new("git")
+        .arg("mob-print")
+        .output()
+        .expect("for now we still depend on git mob-print")
+        .clone()
+        .stdout;
+    let mob = String::from_utf8_lossy(&mob_cmd);
+    let comment_pos = commit_message.find("# ").unwrap();
+    let (git_message, git_comments) = commit_message.split_at(comment_pos);
+    let updated_message = format!("{}{}{}", git_message, mob, git_comments);
+    fs::write(commit_message_file, updated_message).expect("Error writing commit message file");
 }
