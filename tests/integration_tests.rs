814616171c5d9e622c4af1eae19cae0f4505db54
Florian Peter <florian.peter@gmx.at>
2019-03-21 21:01:32 +0800

    Factor out test helper method. #integration-test
    Co-authored-by: Eyal Kalderon <eyal.kalderon@tenx.tech>

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,15 +1,22 @@
 use assert_cmd::prelude::*;
 use git2::Repository;
 use rmob::*;
-use std::error::Error;
 use std::process::Command;
-use tempfile::tempdir;
+use tempfile::{tempdir, TempDir};
 
 #[test]
-fn it_works() -> Result<(), Box<dyn Error>> {
+fn it_works() -> BoxResult {
     let dir = tempdir()?;
     let repo = Repository::init(dir.path())?;
 
+    init(dir)?;
+
+    // TODO: verify hook insertion works on both hashed and unhashed EDIT_COMMIT_MSG
+
+    Ok(())
+}
+
+fn init(dir: TempDir) -> BoxResult {
     let mut rmob = Command::cargo_bin("rmob")?;
     rmob.current_dir(dir.path()).arg("init").assert().success();
     let hook = dir.path().join(".git/hooks/").join(HOOK_NAME);
