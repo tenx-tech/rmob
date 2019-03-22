f875df677c0121e6b0d2efa15d22a9bda8ecfcc7
Florian Peter <florian.peter@gmx.at>
2019-03-22 19:00:37 +0800

    Refactor entrypoint. #refactor-entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+use std::path::Path;
 use std::fs;
 use std::process::Command;
 use std::os::unix::fs::OpenOptionsExt;
@@ -7,6 +11,46 @@ use std::error::Error;
 pub const HOOK_NAME: &str = "prepare-commit-msg";
 
 pub type BoxResult = Result<(), Box<dyn Error>>;
+
+pub fn run() -> BoxResult {
+    let matches = App::new("Rmob")
+        .version("0.1.0")
+        .author("TenX Team <team@tenx.tech>")
+        .about("Arr! Git mobbing in Rust")
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Initialize rmob for this git repo, call this once to use rmob in your git repo")
+                .version("1.0"),
+        )
+        .subcommand(
+            SubCommand::with_name("prepare-commit-msg")
+                .about("Called from the git hook only")
+                .version("1.0")
+                .arg(
+                    Arg::with_name("COMMIT_MESSAGE_FILE")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("prepare-commit-msg") {
+        let commit_message_file = matches.value_of("COMMIT_MESSAGE_FILE").ok_or("Git hook passed a weird param")?;
+        inject_into_commit_message_file(commit_message_file)?;
+    } else if let Some(_) = matches.subcommand_matches("init") {
+        // TODO: Find the path to the top-level git hooks dir from anywhere, use libgit2?
+        let hook_file = ".git/hooks/prepare-commit-msg";
+
+        if Path::new(hook_file).exists() {
+            // TODO: Want to bail! here, do I need a custom error type for that?
+            panic!("You have an existing prepare-commit-msg hook, which we need to overwrite. Please back it up and remove it!");
+        } else {
+            create_hook(hook_file)?;
+        }
+    }
+
+    Ok(())
+}
 
 pub fn create_hook(hook_file: &str) -> io::Result<()> {
     let hook_code = "#!/bin/bash
