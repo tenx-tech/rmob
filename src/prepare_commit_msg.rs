45079ed043e23e55e48726733d607058349a3bf0
Florian Peter <florian.peter@gmx.at>
2019-07-08 01:18:05 +0800

    Many such fun #start
    Co-authored-by: Eyal Kalderon <eyal.kalderon@tenx.tech>
    
    Co-authored-by: Eyal Kalderon <eyal.kalderon@tenx.tech>

--- src/prepare_commit_msg.rs
+++ src/prepare_commit_msg.rs
@@ -4,13 +4,18 @@ use crate::{BoxResult, ACTIVE_COPIRATES_FILE};
 use std::fs;
 
 pub fn inject_into_commit_message_file(commit_message_file: &str) -> BoxResult {
+    const PATTERN: &str = "\n# ";
+
     let commit_message = fs::read_to_string(commit_message_file)?;
     let mob = fs::read_to_string(ACTIVE_COPIRATES_FILE)?;
-    let comment_pos = commit_message
-        .find("# ")
-        .ok_or("No comments found in yer commit, landlover.")?;
+    // Can I transform that into .unwrap_or_else()
+    let comment_pos = if let Some(message) = commit_message.find(PATTERN) {
+        message
+    } else {
+        commit_message.len().saturating_sub(1)
+    };
     let (git_message, git_comments) = commit_message.split_at(comment_pos);
-    let updated_message = format!("{}{}{}", git_message, mob, git_comments);
+    let updated_message = format!("{}\n\n{}{}", git_message, mob, git_comments);
 
     fs::write(commit_message_file, updated_message)?;
 
