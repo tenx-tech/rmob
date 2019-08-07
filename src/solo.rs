d3ae16b1d2f236c505d2fac93571e515f0b7540e
Eyal Kalderon <eyal.kalderon@tenx.tech>
2019-08-07 15:24:07 +0800

    Add missing doc comment to solo()

--- src/solo.rs
+++ src/solo.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 use crate::active_copirate::ActiveCoPirates;
 use crate::BoxResult;
 
+/// Disable mobbing on the Git repository located at `repo_dir`.
 pub fn solo(repo_dir: &Path) -> BoxResult<()> {
     ActiveCoPirates::create_empty(repo_dir)?;
 
