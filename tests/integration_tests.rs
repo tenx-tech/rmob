39563225361c0a6ed12f0b723e97b3e23b69ebf1
Evgenii Ponomarev <eupn@protonmail.com>
2019-09-18 15:34:53 +0800

    Add co-pirate via command in tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -13,7 +13,7 @@ fn it_works() -> BoxResult<()> {
 
     embark(&dir)?;
 
-    init_git_copirates(&dir)?;
+    add_new_copirate(&dir)?;
 
     sail(&dir)?;
 
@@ -38,20 +38,18 @@ fn embark(dir: &TempDir) -> BoxResult<()> {
     Ok(())
 }
 
-fn init_git_copirates(dir: &TempDir) -> BoxResult<()> {
-    let copirates = r#"
-        {
-          "copirates": {
-            "cb": {
-              "name": "Charlotte de Berry",
-              "email": "berrydeath@england.pir"
-            }
-          }
-        }
-    "#;
-    let copirates_file = dir.path().join(".git-copirates");
-
-    fs::write(copirates_file, copirates)?;
+fn add_new_copirate(dir: &TempDir) -> BoxResult<()> {
+    let mut rmob = Command::cargo_bin("rmob")?;
+    rmob.current_dir(dir.path())
+        .arg("--git-copirates-file")
+        .arg(dir.path().join(".git-copirates"))
+        .arg("copirate")
+        .arg("add")
+        .arg("cb")
+        .arg("'Charlotte de Berry'")
+        .arg("'berrydeath@england.pir'")
+        .assert()
+        .success();
 
     Ok(())
 }
